228c995be8425a629a3ae6168b7a9e823c6a34f3
alphabet02 <hoanglamdung13092005@gmail.com>
2026-02-25 22:38:56 +0700

    Add files via upload

---
+++ BtvnBuoi9.java
@@ -0,0 +1,144 @@
+package btvn_buoi9;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class BtvnBuoi9 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//bai1
+//		System.out.println("nhap vao so n");
+//		int n = scanner.nextInt(); 
+//		if(n>=0) System.out.println("day la so nguyen duong");
+//		else System.out.println("day la so nguyen am");
+//		
+//		//bai1.1
+//		System.out.println("nhap vao so n");
+//		int n1 = scanner.nextInt(); 
+//		if(n%2==0) System.out.println("day la so chan");
+//		else System.out.println("day la so le");
+//		
+//		
+//		//bai1.2
+//		System.out.println("nhap vao so n");
+//		double n2 = scanner.nextInt(); 
+//		if(n2 < 1) System.out.println("ko phai so nguyen to");
+//		for(int i = 2; i < Math.sqrt(n2); i++) {
+//            if (n2 % i == 0) {
+//                System.out.println("ko phai so nguyen to");
+//                return;
+//            }
+//        }
+//		System.out.println("la so nguyen to");
+        	
+
+//		//bai 2
+//		
+//		int so = scanner.nextInt();
+//		switch(so) {
+//			case 1:
+//				System.out.println("một");
+//				break;
+//			case 2:
+//				System.out.println("hai");
+//				break;
+//			case 3:
+//				System.out.println("ba");
+//				break;
+//			case 4:
+//				System.out.println("bốn");
+//				break;
+//			case 5:
+//				System.out.println("năm");
+//				break;
+//			case 6:
+//				System.out.println("sáu");
+//				break;
+//			case 7:
+//				System.out.println("bảy");
+//				break;
+//			case 8:
+//				System.out.println("tám");
+//				break;
+//			case 9:
+//				System.out.println("chín");
+//				break;
+//			case 10:
+//				System.out.println("-mười");
+//				break;	
+//			default:
+//				System.out.println("ko có số nào ở trên");
+//		}
+//		
+//		//bai 3
+//		int n = scanner.nextInt();
+//		int sum = 0;
+//		for(int i = 0 ; i<n;i++) {
+//			if(i%2==0) sum+=i;
+//		}
+//		System.out.println(sum);
+//		
+//		//bai 4
+//		int n1 = scanner.nextInt();
+//		int [] mang = new int [n1];
+//		for(int i = 0; i < n1; i++) {
+//			System.out.println("nhap phan tu thu " + i);
+//			mang[i] = scanner.nextInt(); 
+//		}
+//		int avg = 0;
+//		int sum1=0;
+//		for(int i=0; i<n1; i++) {
+//			sum+=mang[i];
+//		}
+//		avg = sum1 / n1;
+//		System.out.println(avg);
+		
+		
+		//bai 5
+//		int n1 = scanner.nextInt();
+//		int [] mang = new int [n1];
+//		for(int i = 0; i < n1; i++) {
+//			System.out.println("nhap phan tu thu " + i);
+//			mang[i] = scanner.nextInt(); 
+//		}
+//		int max = mang[0];
+//		for(int i=1; i<n1; i++) {
+//			if(mang[i] > max) max = mang[i];
+//		}
+//		System.out.println(max);
+//
+//		int min = mang[0];
+//		for(int i=1; i<n1; i++) {
+//			if(mang[i] < min) min = mang[i];
+//		}
+//		System.out.println(min);
+//		
+		//bai 6
+//		System.out.println("nhap vao canh");
+//		int canh = scanner.nextInt();
+//		tinhChuVi(canh);
+//		tinhDienTich(canh);
+//		
+//		//bai 7
+//		int n1 = scanner.nextInt();
+//		int [] mang = new int [n1];
+//		for(int i = 0; i < n1; i++) {
+//		System.out.println("nhap phan tu thu " + i);
+//			mang[i] = scanner.nextInt(); 
+//		}
+//		
+//		for(int i = 0; i < n1; i++) {
+//			System.out.println(mang[i] + " ");
+//			}
+//		//bai 8
+//		int n1 = scanner.nextInt();
+//		int [] mang = new int [n1];
+//		for(int i = 0; i < n1; i++) {
+//		System.out.println("nhap phan tu thu " + i);
+//			mang[i] = scanner.nextInt(); 
+//		}
+//		for(int i = 0; i < n1; i++) {
+//			if(mang[i]%2==0)
+//			System.out.println(mang[i] + " ");
+//			}
+
+}
